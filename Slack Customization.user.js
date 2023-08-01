092a3467377bbac3cf487def7bccf029b1452bab
allarounddev <allarounddeveloper@outlook.com>
2023-08-01 21:56:50 +0900

    updated script

--- Slack Customization.user.js
+++ Slack Customization.user.js
@@ -17,6 +17,8 @@
         const unmutedChannels = document.querySelectorAll('[data-qa-channel-sidebar-channel-is-muted^="false"]');
         const mutedChannels = document.querySelectorAll('[data-qa-channel-sidebar-channel-is-muted^="true"]');
         const mutedSections = document.querySelectorAll('[data-qa^="muted_channels"]');
+        const colabSections = document.querySelectorAll('[data-qa^="collab"]');
+
 
         mutedChannels.forEach(channel => {
             const styleTag = document.head.appendChild(document.createElement('style'));
@@ -29,12 +31,10 @@
 
             section.parentNode.style.display = 'none';
         });
+        colabSections.forEach(section => {
 
-        unmutedChannels.forEach(channel => {
-            channel.parentNode.style.display = '';
+            section.parentNode.style.display = 'none';
         });
-
-
 
     }
 
@@ -43,7 +43,7 @@
     function removeBoldingBadges() {
         const mutedChannels = document.querySelectorAll('[data-qa^="channel_sidebar_name"]');
         const sideSections = document.querySelectorAll('.p-channel_sidebar__section_heading_label');
-        console.log(mutedChannels);
+
         mutedChannels.forEach(channel => {
             channel.style.fontWeight = 'normal';
             channel.style.color = 'unset';
@@ -57,7 +57,7 @@
 
     // Function to remove red badges
     function removeRedBadges() {
-        const badges = document.querySelectorAll('.p-channel_sidebar__badge, .p-channel_sidebar__badge_count, .p-channel_sidebar__badge--red');
+        const badges = document.querySelectorAll('.p-channel_sidebar__badge, .p-channel_sidebar__badge_count, .p-channel_sidebar__badge--red, .p-recent_page__conversation__secondary');
 
         badges.forEach(badge => {
             badge.style.display = 'none';
@@ -70,21 +70,55 @@
 
         var spacer = document.querySelector(".p-channel_sidebar__pages_list_spacer");
         var filteredItems = [];
+        var filteredItemsForPrimary = [];
+        var tempArray = Object.values(items);
+        var spacer1 = document.querySelector('[data-qa^="direct_messages"]');
+        var spacer2 = document.querySelector('[data-qa^="primary_focus"]');
 
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
+            //tempArray.push(item);
             if (!(item.compareDocumentPosition(spacer) & Node.DOCUMENT_POSITION_PRECEDING)) {
                 filteredItems.push(item);
+                tempArray = tempArray.filter(v => v !== item);
+
             }
         }
 
+        for (var j = 0; j < tempArray.length; j++) {
+            var item1 = tempArray[j];
+            if (!(item1.compareDocumentPosition(spacer2) & Node.DOCUMENT_POSITION_FOLLOWING)) {
+                if (!(item1.compareDocumentPosition(spacer1) & Node.DOCUMENT_POSITION_PRECEDING)) {
+                    filteredItemsForPrimary.push(item1);
+                }
+            }
+        }
+        console.log(tempArray);
+        console.log(filteredItemsForPrimary);
         // Output the filtered items
         filteredItems.forEach(function(item) {
             if(!(item.querySelector(".p-channel_sidebar__pages_list_spacer") || item.querySelector(".p-channel_sidebar__name")?.textContent == "Later" || item.querySelector(".p-channel_sidebar__name")?.textContent == "Canvases")){
-                //item.remove();
                 item.style.display = 'none';
             }
         });
+        for (var k = 0; k < tempArray.length; k++) {
+            console.log('itemIndex', k);
+            for (var l = 0; l < filteredItemsForPrimary.length; l++) {
+                console.log('filterIndex', l);
+                if (tempArray[k].id == filteredItemsForPrimary[l].id) {
+                    if (!tempArray[k].id.includes('sectionHeading')) {
+                        tempArray[k].style.display = 'none';
+                        console.log('okokokok');
+                        console.log(tempArray[k].id);
+                        console.log(filteredItemsForPrimary[l].id);
+                    }
+                    //break;
+                }
+            }
+            console.log('breaked!');
+        }
+
+
     }
 
     // Function to hide "Unread messages" scroll bar
@@ -96,6 +130,23 @@
         }
     }
 
+    // Function to hide "Unread Badge" when scroll
+    function hideUnreadBadge() {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.addedNodes.length > 0) {
+                    const unreadBadge = document.querySelector('.p-channel_sidebar__banner--unreads');
+                    if (unreadBadge) {
+                        unreadBadge.style.display = 'none';
+                    }
+                }
+            });
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
 
     // Call all the customization functions
     setInterval(hideUnreadScrollBar, 1000);
@@ -103,4 +154,5 @@
     setInterval(hideTopSections, 1000);
     setInterval(removeBoldingBadges, 1000);
     setInterval(removeMutedChannels, 1000);
+    setInterval(hideUnreadBadge, 1000);
 })();
